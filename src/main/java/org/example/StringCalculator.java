c4ff8f4553d1156d4be18308dc04060d8a6ed792
Preet Brahmbhatt <preet.brahmbhatt.code@gmail.com>
2025-07-04 21:13:39 +0530

    [Test 1.1 Passed] Added check for empty string for add method(Green Testcase).

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,6 +4,10 @@ public class StringCalculator
 {
     public int add(String inputSequence)
     {
+        if (inputSequence == null || inputSequence.isEmpty())
+        {
+            return 0;
+        }
         return -1;
     }
 }
